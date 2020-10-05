7d2bfc19e1dd5d54dc8fb4fad4441f4fc9fbd6ea
Nitzan <nitzankl27@gmail.com>
2020-10-05 13:54:46 +0300

    updated mocks file

--- utils/constants/mocks.js
+++ utils/constants/mocks.js
@@ -1,8 +1,9 @@
 import * as muiColors from "@material-ui/core/colors";
 import { OperationPostTypes } from "db/models/post";
 
-const LONG_STRING_WITH_LINE_BREAKS =
-    "testing\n 1 2 3\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the";
+const LONG_STRING =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the";
+const LONG_STRING_WITH_LINE_BREAKS = `testing\n 1 2 3\n${LONG_STRING}`;
 
 const CURRENT_TIME_STRING = new Date().toISOString();
 
@@ -67,9 +68,23 @@ export const DUMMY_OPERATION_POSTS = [
     {
         author: ALL_USERS[1],
         description: "testing testing",
-        type: OperationPostTypes.UPDATE,
+        type: OperationPostTypes.ACTION,
         writtenAt: "2020-08-28T11:42:47.237Z",
         happenedAt: "2020-08-28T11:42:47.237Z",
         attachments: ["lala.txt", "test.clsx", "this_is_a_long_one_lolololol.idk"],
+    },
+    {
+        author: ALL_USERS[1],
+        description: "testing testing",
+        type: OperationPostTypes.SUCCESS,
+        writtenAt: "2020-08-28T11:42:47.237Z",
+        happenedAt: "2020-08-28T11:42:47.237Z",
+    },
+    {
+        author: CURRENT_USER,
+        description: LONG_STRING + LONG_STRING,
+        type: OperationPostTypes.RECON,
+        writtenAt: "2020-08-28T11:42:47.237Z",
+        happenedAt: "2020-08-28T11:42:47.237Z",
     },
 ];
