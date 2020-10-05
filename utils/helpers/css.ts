78c8148ee7eaf5532d297558a3b97464ba8aba7d
Nitzan <nitzankl27@gmail.com>
2020-10-05 19:01:17 +0300

    added custom scrollbar!

--- utils/helpers/css.ts
+++ utils/helpers/css.ts
@@ -50,3 +50,12 @@ export function isElementOverflowing(el: HTMLElement): boolean {
     if (!el) return false;
     return el.offsetHeight < el.scrollHeight;
 }
+
+export function hexToRGB(hex: string, alpha: number): string {
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5, 7), 16);
+
+    if (alpha) return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+    else return `rgba(${r}, ${g}, ${b})`;
+}
