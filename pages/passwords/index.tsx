e9c5aa862e2745e5ce5fb48a170e5abac05b661c
Nitzan Kletter <nitzan.kletter@gmail.com>
2020-10-23 16:23:06 +0300

    removed the test table in Passwords page

--- pages/passwords/index.tsx
+++ pages/passwords/index.tsx
@@ -1,27 +1,4 @@
 import PageLayout from "components/layouts/MainLayout";
-import Table from "components/general/Table";
 
-const Passwords = () => <PageLayout>
-    <Table
-              columns={[
-                { title: "Adı", field: "name" },
-                { title: "Soyadı", field: "surname" },
-                { title: "Doğum Yılı", field: "birthYear", type: "numeric" },
-                {
-                  title: "Doğum Yeri",
-                  field: "birthCity",
-                  lookup: { 34: "İstanbul", 63: "Şanlıurfa" },
-                },
-              ]}
-              data={[
-                {
-                  name: "Mehmet",
-                  surname: "Baran",
-                  birthYear: 1987,
-                  birthCity: 63,
-                },
-              ]}
-              title="Demo Title"
-     />
-</PageLayout>;
+const Passwords = () => <PageLayout>Passwords</PageLayout>;
 export default Passwords;
