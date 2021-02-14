b61fbd2f5bc37369a0855909ccd120c9dee85b5e
Nitzan Kletter <nitzan.kletter@gmail.com>
2021-02-14 11:53:58 +0200

    post-attachments storage folder is now read-only

--- src/pages/storage/file-manager.tsx
+++ src/pages/storage/file-manager.tsx
@@ -13,6 +13,8 @@ const CUSTOM_FILE_VIEWER_CAPABILITY = {
     availableInContexts: ["row", "toolbar"],
 };
 
+const unpatchedCapabilities = ["sort", "download"];
+
 export default function FileManager() {
     const [previewedFile, setPreviewedFile] = useState("");
 
@@ -34,23 +36,52 @@ export default function FileManager() {
                     id="filemanager-1"
                     api={connectorNodeV1.api}
                     apiOptions={apiOptions}
-                    capabilities={(apiOptions, actions) => [
-                        // basic capabilities
-                        ...connectorNodeV1.capabilities(apiOptions, actions),
-                        // custom capabilities
-                        {
-                            ...CUSTOM_FILE_VIEWER_CAPABILITY,
-                            handler: () => {
-                                const selected = actions.getSelectedResources();
-                                if (selected.length !== 1) return;
-                                onDoubleClickItem({ rowData: selected[0] });
+                    capabilities={(apiOptions, actions) => {
+                        const basicCapabilities = connectorNodeV1.capabilities(apiOptions, actions);
+                        let patchedCapabilities = [];
+
+                        // This next piece of code makes it impossible UI-wise to make any changes to the
+                        // post-attachments directory, making it read-only. The only capabilities that should
+                        // work are file download/preview.
+                        for (const capability of basicCapabilities) {
+                            if (unpatchedCapabilities.includes(capability.id)) patchedCapabilities.push(capability);
+                            else
+                                patchedCapabilities.push({
+                                    ...capability,
+                                    shouldBeAvailable: () => {
+                                        const selected = actions.getSelectedResources();
+                                        if (selected.length === 0) {
+                                            if (actions.getResource().name !== "post-attachments") return capability.shouldBeAvailable();
+                                            return false;
+                                        }
+                                        if (
+                                            !selected.some(
+                                                (s) =>
+                                                    s.name === "post-attachments" || s.ancestors[s.ancestors.length - 1].name === "post-attachments"
+                                            )
+                                        )
+                                            return capability.shouldBeAvailable();
+                                        return false;
+                                    },
+                                });
+                        }
+
+                        return [
+                            ...patchedCapabilities,
+                            {
+                                ...CUSTOM_FILE_VIEWER_CAPABILITY,
+                                handler: () => {
+                                    const selected = actions.getSelectedResources();
+                                    if (selected.length !== 1) return;
+                                    onDoubleClickItem({ rowData: selected[0] });
+                                },
+                                shouldBeAvailable: () => {
+                                    const selected = actions.getSelectedResources();
+                                    return selected.length === 1 && selected[0].type === "file";
+                                },
                             },
-                            shouldBeAvailable: () => {
-                                const selected = actions.getSelectedResources();
-                                return selected.length === 1 && selected[0].type === "file";
-                            },
-                        },
-                    ]}
+                        ];
+                    }}
                     listViewLayout={connectorNodeV1.listViewLayout}
                     viewLayoutOptions={connectorNodeV1.viewLayoutOptions}
                     onResourceItemDoubleClick={onDoubleClickItem}
