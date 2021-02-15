8c3a3ed802718289e549317eab1e2db17ceae20a
Nitzan Kletter <nitzan.kletter@gmail.com>
2021-02-15 14:18:13 +0200

    Finished post attachments UI + fixed overflows

--- src/components/general/Menu.tsx
+++ src/components/general/Menu.tsx
@@ -3,6 +3,8 @@ import { withStyles, createStyles } from "@material-ui/core/styles";
 import Popover, { PopoverProps } from "@material-ui/core/Popover";
 import MenuItem from "@material-ui/core/MenuItem";
 import { SvgIconComponent } from "@material-ui/icons";
+import Typography from "@material-ui/core/Typography";
+import clsx from "clsx";
 
 import { MuiStyles } from "src/utils/interfaces";
 
@@ -15,6 +17,9 @@ const styles = () =>
         },
         menuItemIcon: {
             marginRight: 12,
+        },
+        smallMenuItem: {
+            padding: "2px 10px",
         },
     });
 
@@ -29,19 +34,23 @@ type MenuProps = MuiStyles &
         anchorEl: PopoverProps["anchorEl"];
         onClose: PopoverProps["onClose"];
         items: MenuItemProps[];
+        size?: "regular" | "small";
     };
 
-function Menu(props: MenuProps) {
-    const { classes, items, ...popoverProps } = props;
-
+function Menu({ classes, items, size = "regular", ...popoverProps }: MenuProps) {
     return (
         <Popover {...popoverProps}>
             {items.length > 0 &&
                 items.map(({ title, icon: Icon, onClick }, index) => (
-                    <MenuItem key={title} divider={index !== items.length - 1} onClick={onClick}>
+                    <MenuItem
+                        key={title}
+                        divider={index !== items.length - 1}
+                        onClick={onClick}
+                        className={clsx(size === "small" && classes.smallMenuItem)}
+                    >
                         <div className={classes.menuItemWrapper}>
                             {Icon ? <Icon className={classes.menuItemIcon} /> : null}
-                            {title}
+                            <Typography variant={size === "small" ? "caption" : "body1"}>{title}</Typography>
                         </div>
                     </MenuItem>
                 ))}
--- src/pages/operations/timeline/post/post-attachments.tsx
+++ src/pages/operations/timeline/post/post-attachments.tsx
@@ -4,17 +4,18 @@ import AttachFileIcon from "@material-ui/icons/AttachFile";
 import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
 import Typography from "@material-ui/core/Typography";
 import connectorNodeV1 from "@opuscapita/react-filemanager-connector-node-v1";
+import Hidden from "@material-ui/core/Hidden";
 
-import { spaceChildren } from "src/utils/helpers/css";
+import { usePopoverState } from "src/utils/hooks";
+import Menu from "src/components/general/Menu";
 
 const storageApiOptions = { apiRoot: `http://localhost:${process.env.STORAGE_PORT}` };
 
 const styles = (theme: Theme) =>
     createStyles({
-        attachments: {
+        root: {
             display: "flex",
             alignItems: "center",
-            ...spaceChildren("horizontally", 8),
         },
         icons: { display: "flex" },
         icon: {
@@ -23,9 +24,15 @@ const styles = (theme: Theme) =>
         attachmentChip: {
             backgroundColor: lighten(theme.constants.appBackgroundHighlight, 0.1),
             borderRadius: 4,
-            padding: "2px 8px",
+            padding: "2px 5px",
             cursor: "pointer",
-            // TODO: give max width and handle text that is too long
+        },
+        attachmentChipsDiv: {
+            "& > *": {
+                margin: 2,
+            },
+            display: "flex",
+            flexWrap: "wrap",
         },
     });
 
@@ -34,6 +41,56 @@ type StoredAttachment = {
     name: string;
     ancestors: { name: string }[];
 };
+
+type AttachmentChipProps = WithStyles<typeof styles> & {
+    onClick: (attachmentPath: string) => void;
+    attachment: StoredAttachment;
+    children?: React.ReactChild;
+};
+
+const AttachmentChip = withStyles(styles)(({ classes, onClick, attachment, children }: AttachmentChipProps) => (
+    <Typography
+        key={attachment.id}
+        title="Click for file preview"
+        variant="caption"
+        className={classes.attachmentChip}
+        onClick={() => onClick(["", ...attachment.ancestors.map((a) => a.name).slice(1), attachment.name].join("/"))}
+    >
+        {children ||
+            (attachment.name.length > 15 ? `${attachment.name.slice(0, 7)}...${attachment.name.slice(attachment.name.length - 7)}` : attachment.name)}
+    </Typography>
+));
+
+type AttachmentsPreviewChipProps = WithStyles<typeof styles> & {
+    onClickAttachment: (attachmentPath: string) => void;
+    attachments: StoredAttachment[];
+};
+
+const AttachmentsPreviewChip = withStyles(styles)(({ classes, onClickAttachment, attachments }: AttachmentsPreviewChipProps) => {
+    const [anchorEl, setAnchorEl, clearAnchorEl] = usePopoverState();
+
+    return (
+        <>
+            <Typography variant="caption" className={classes.attachmentChip} onClick={setAnchorEl} title="Click to preview attachments">
+                <b>+ {attachments.length} More</b>
+            </Typography>
+            <Menu
+                size="small"
+                anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                }}
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={clearAnchorEl}
+                items={attachments.map((attachment) => ({
+                    title: attachment.name,
+                    onClick: () => onClickAttachment(["", ...attachment.ancestors.map((a) => a.name).slice(1), attachment.name].join("/")),
+                }))}
+            />
+        </>
+    );
+});
 
 type PostAttachmentsProps = WithStyles<typeof styles> & { postId: string; previewAttachment: (attachmentPath: string) => void };
 
@@ -57,22 +114,25 @@ function PostAttachments({ classes, postId, previewAttachment }: PostAttachments
     if (attachments.length === 0) return null;
 
     return (
-        <div className={classes.attachments}>
+        <div className={classes.root}>
             <div className={classes.icons}>
                 <AttachFileIcon className={classes.icon} />
                 <KeyboardArrowRightIcon className={classes.icon} />
             </div>
-            {attachments.map((attachment) => (
-                <Typography
-                    key={attachment.id}
-                    title="Click for file preview"
-                    variant="caption"
-                    className={classes.attachmentChip}
-                    onClick={() => previewAttachment(["", ...attachment.ancestors.map((a) => a.name).slice(1), attachment.name].join("/"))}
-                >
-                    {attachment.name}
-                </Typography>
-            ))}
+            <div className={classes.attachmentChipsDiv}>
+                <AttachmentChip attachment={attachments[0]} onClick={previewAttachment} />
+                {attachments[1] && (
+                    <Hidden mdDown>
+                        <AttachmentChip attachment={attachments[1]} onClick={previewAttachment} />
+                        {attachments.length > 2 && (
+                            <AttachmentsPreviewChip attachments={attachments.slice(2)} onClickAttachment={previewAttachment} />
+                        )}
+                    </Hidden>
+                )}
+                <Hidden lgUp>
+                    {attachments.length > 1 && <AttachmentsPreviewChip attachments={attachments.slice(1)} onClickAttachment={previewAttachment} />}
+                </Hidden>
+            </div>
         </div>
     );
 }
